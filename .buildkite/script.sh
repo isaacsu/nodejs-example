f873c36c95cfe28824476b2c3d86474148a2d7be
Isaac Su <me@isaacsu.com>
2026-05-13 16:11:14 +1000

    fix id and bucket url

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-export BUILDKITE_CACHE_BUCKET_URL="s3://isaac-bucket/prefix?endpoint=http://localhost:9000&use_path_style=true"
+export BUILDKITE_CACHE_BUCKET_URL="s3://isaac-bucket/prefix?endpoint=http://minio.buildkite.orb.local:9000&use_path_style=true"
 
 buildkite-agent cache restore
 npm install
